7929049bf992564f2c182a6b6e92b8d0662fcc98
wangxiyou <wangxiyou@126.com>
2019-12-23 15:22:44 +0800

    Create

---
+++ src/PB.PLBS.Domain.Core/Bases/INamedObject.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PB.PLBS.Domain
+{
+    /// <summary>
+    /// 定义一个可命名对象的接口
+    /// 可命名对象具有编号和名称属性，并可以用编号作为唯一索引
+    /// </summary>
+    public interface INamedObject
+    {
+    }
+}
---
+++ src/PB.PLBS.Domain.Core/Bases/NamedObjectDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PB.PLBS.Domain
+{
+    /// <summary>
+    /// 提供 <see cref="INamedObject"/> 对象的数据迁移对象
+    /// </summary>
+    public struct NamedObjectDTO
+    {
+    }
+}
